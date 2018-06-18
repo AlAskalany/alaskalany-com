c983ba00dff62e9d6e7a6a0ae58e2cab9f123568
Ahmed AlAskalany <ahmed.alaskalany@gmail.com>
2018-06-18 17:14:24 +0200

    Used "AppHeader" React component in main page
    Used the newly created "AppHeader" React component in App.tsx, as a header for the main page.

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,18 @@
-import * as React from 'react';
-import './App.css';
+import * as React from "react";
+import "./App.css";
 
-import logo from './logo.svg';
+import AppHeader from "./components/AppHeader/AppHeader";
+import Hello from "./components/Hello/Hello";
 
 class App extends React.Component {
   public render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
+        <AppHeader />
         <p className="App-intro">
           To get started, edit <code>src/App.tsx</code> and save to reload.
         </p>
+        <Hello name="Askalany" />
       </div>
     );
   }
