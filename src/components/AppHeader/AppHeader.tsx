5e8ab2b2c2b7fd91d0b0417ecbd248268bc00efe
Ahmed AlAskalany <ahmed.alaskalany@gmail.com>
2018-06-18 17:13:26 +0200

    Created "AppHeader" React component
    Created a class-based React component called "AppHeader" to be used as a header for the website, and could be reused across different pages.

---
+++ src/components/AppHeader/AppHeader.tsx
@@ -0,0 +1,22 @@
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import * as React from "react";
+
+class AppHeader extends React.Component {
+  public render() {
+    return (
+      <div>
+        <AppBar position="static" color="default">
+          <Toolbar>
+            <Typography variant="title" color="inherit">
+              Title
+            </Typography>
+          </Toolbar>
+        </AppBar>
+      </div>
+    );
+  }
+}
+
+export default AppHeader;
